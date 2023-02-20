ef2fad3bd14b723dc7afb39b2a4e4d39d3aacd19
sagor-sheikh <wpsagor@gmail.com>
2023-02-21 00:07:58 +0600

    ready website

---
+++ src/component/Hero/Home.js
@@ -0,0 +1,68 @@
+import React from "react"
+import "./Home.css"
+import hero from "../pic/hero.png"
+import skill1 from "../pic/skill1.png"
+import skill2 from "../pic/skill2.png"
+import skill3 from "../pic/skill3.png"
+import { Typewriter } from "react-simple-typewriter"
+
+const Home = () => {
+  return (
+    <>
+      <section className='hero' id='home'>
+        <div className='container f_flex top'>
+          <div className='left top'>
+            <h3>WELCOME TO MY WORLD</h3>
+            <h1>
+              Hi, I’m <span>Sagor Sheikh</span>
+            </h1>
+            <h2>
+              a
+              <span>
+                <Typewriter words={[" Professional Coder.", " Developer."]} loop cursor cursorStyle='|' typeSpeed={70} deleteSpeed={50} delaySpeed={1000} />
+              </span>
+            </h2>
+
+            <p>Experienced React.js developer with over 3 years of experience creating scalable and efficient web applications. Proficient in React.js, Redux, Node.js, and other front-end technologies, with a strong understanding of web development principles and best practices. Passionate about building user-friendly and responsive interfaces that deliver exceptional user experiences. Committed to continuously learning and staying up-to-date with the latest technologies and trends in the industry.</p>
+
+            <div className='hero_btn d_flex'>
+              <div className='col_1'>
+                <h4>FIND WITH ME</h4>
+                <div className='button'>
+                  <button className='btn_shadow'>
+                    <i class='fab fa-facebook-f'></i>
+                  </button>
+                  <button className='btn_shadow'>
+                    <i class='fab fa-instagram'></i>
+                  </button>
+                  <button className='btn_shadow'>
+                    <i class='fab fa-linkedin-in'></i>
+                  </button>
+                </div>
+              </div>
+              <div className='col_1'>
+                <h4>BEST SKILL ON</h4>
+                <button className='btn_shadow'>
+                  <img src={skill1} alt='' />
+                </button>
+                <button className='btn_shadow'>
+                  <img src={skill2} alt='' />
+                </button>
+                <button className='btn_shadow'>
+                  <img src={skill3} alt='' />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='right'>
+            <div className='right_img'>
+              <img src={hero} alt='' />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Home
